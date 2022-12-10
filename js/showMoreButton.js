0c7c640d0d2bad7d851135bd27a16822f6bc900a
Leandro Fonseca <leandro_dinis@hotmail.com>
2022-12-10 23:53:04 +0000

    FIX showMoreButton.js

--- js/showMoreButton.js
+++ js/showMoreButton.js
@@ -1,17 +1,24 @@
+const number = 3;
 
-    let w = document.querySelectorAll(".content-wrapper");
+let w = document.querySelectorAll(".content-wrapper");
     for (let i = 0; i < w.length; i++) {
         let c = w[i].querySelectorAll(".content");
-        $(c).slice(0,3).show();
+        $(c).slice(0,number).show();
+        if(c.length <= number){
+            noMoreContentButton(w[i].querySelector(".show-more"));
+        }
     }
 
-    function a(button){
-        const number = 3;
+    function noMoreContentButton(button){
+        button.innerHTML = "Mais nada para mostrar";
+        button.classList = "show-more noContent";
+    }
+
+    function showMoreContent(button){
         let contentWrapper = button.parentNode.parentNode;
         let contents = $(contentWrapper).find('.content:hidden');
         contents.slice(0, number).slideDown();
         if(contents.length <= number){
-            button.innerHTML = "Mais nada para mostrar";
-            button.classList = "show-more noContent";
+            noMoreContentButton(button);
         }
     }
