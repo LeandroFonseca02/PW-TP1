f0522f9048ba1318c8b05d31cb361684b17288f8
Leandro Fonseca <leandro_dinis@hotmail.com>
2022-12-10 23:33:15 +0000

    FIX showMoreButton.js

--- js/showMoreButton.js
+++ js/showMoreButton.js
@@ -1,10 +1,17 @@
-$(document).ready(function () {
-    $(".content").slice(0, 5).show();
-    $("#showMore").on("click", function (e) {
-        e.preventDefault();
-        $(".content:hidden").slice(0, 5).slideDown();
-        if ($(".content:hidden").length == 0) {
-            $("#showMore").text("Mais nada para mostrar").addClass("noContent");
+
+    let w = document.querySelectorAll(".content-wrapper");
+    for (let i = 0; i < w.length; i++) {
+        let c = w[i].querySelectorAll(".content");
+        $(c).slice(0,3).show();
+    }
+
+    function a(button){
+        const number = 3;
+        let contentWrapper = button.parentNode.parentNode;
+        let contents = $(contentWrapper).find('.content:hidden');
+        contents.slice(0, number).slideDown();
+        if(contents.length <= number){
+            button.innerHTML = "Mais nada para mostrar";
+            button.classList = "show-more noContent";
         }
-    });
-});
+    }
